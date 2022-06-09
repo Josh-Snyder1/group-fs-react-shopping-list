1139f95b49941aea048ae12a2df26de6769c5171
Nimo Abdi <nimoabdi318@gmail.com>
2022-06-09 11:37:50 -0500

    GET finished

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
-
 import Header from '../Header/Header.jsx'
 import ShoppingList from '../ShoppingList/ShoppingList'
 import './App.css';
 
 
 function App() {
+    let [shoppingListItem, setShoppingList] = useState('');
+    const getNewItem = () => {
+
+        axios({
+            method: 'GET',
+            url: '/items'
+        }).then((res)=>{
+            console.log('res.data', res.data);
+            setShoppingList(res.data)
+        })
+        .catch((error)=> {
+            console.log('GET items failed', error);
+        });
+    };
     return (
         <div className="App">
             <Header />
--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,7 +1,24 @@
-function ShoppingList() {
+function ShoppingList({shoppingList}) {
+ console.log(ShoppingList);
     return (
         <>
+        <h2>Shopping List</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Items</th>
+                </tr>
+            </thead>
+            <tbody>
+                {shoppingList.map((shoppingList)=>
+                <tr key={shoppingList.id}>
+                    <td>{shoppingList.name}</td>
+
+                </tr>
+            )}
+            </tbody>
+        </table>
         </>
     )
-}
+}               
 export default ShoppingList;
