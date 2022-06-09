814832fddd88c330cba8d122a082100d9757b64e
Josh Snyder <joshrsnyder@gmail.com>
2022-06-09 10:18:17 -0500

    changed router name

--- server/routes/item.router.js
+++ server/routes/item.router.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
-
+//use '/item' for all endpoints
 
 module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from '../Header/Header.jsx'
 import './App.css';
 
+//use '/item' for all endpoints
 
 function App() {
     return (
